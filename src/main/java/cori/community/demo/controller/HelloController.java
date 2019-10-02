579b8fa7fe5d2b913fae6ed6f21d6ecf8c30e07a
z3plus2 <z3plus2@gmail.com>
2019-10-02 12:10:01 +0800

    2.搭载基础页面

--- src/main/java/cori/community/demo/controller/HelloController.java
+++ src/main/java/cori/community/demo/controller/HelloController.java
@@ -3,7 +3,6 @@ package cori.community.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -14,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-
-        model.addAttribute("name", name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
